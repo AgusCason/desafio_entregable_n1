b90ebb03883ca2ef98f00a402619105ff54bf814
AgusCason <88736454+AgusCason@users.noreply.github.com>
2022-07-21 11:18:18 -0300

    Add files via upload

---
+++ desafio_n1.js
@@ -0,0 +1,72 @@
+/* EJERCICIO N1 - CICLO FOR   
+
+debugger ;
+
+let numero = prompt("TABLAS MATEMATICAS. Ingrese el numero de la tabla que desea ver:") ;
+
+let resultado = 0 ;
+
+console.log( "TABLA DEL ", numero) ;
+
+for( let n = 0 ; n <= 10 ; n++ ){
+    
+    resultado = numero * n ;
+
+    console.log( numero , "x" , n , "=" , resultado) ;
+   
+}
+
+*/
+
+
+
+/* EJERCICIO N2 - CICLO WHILE  
+
+debugger ;
+
+let numero = parseInt(prompt("TABLAS MATEMATICAS. Ingrese el numero de la tabla que desea ver:")) ;
+
+let resultado = 0 ;
+
+let n = 0 ;
+
+console.log( "TABLA DEL ", numero) ;
+
+while( n <= 10 ){
+    
+    resultado = numero * n ;
+
+    console.log( numero , "x" , n , "=" , resultado) ;
+
+    n++ ;
+    
+}
+
+*/
+
+
+/* EJERCICIO N3 - CICLO  DO-WHILE 
+
+debugger ;
+
+let numero = parseInt(prompt("TABLAS MATEMATICAS. Ingrese el numero de la tabla que desea ver:")) ;
+
+let resultado = 0 ;
+
+let n = 0 ;
+
+console.log( "TABLA DEL ", numero) ;
+
+do{
+    
+    resultado = numero * n ;
+
+    console.log( numero , "x" , n , "=" , resultado) ;
+
+    n++ ;
+    
+}while( n <= 10 ) ;
+
+
+*/
+
